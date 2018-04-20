3de8912e2f5a98c56addc4c2714c3ac4fd2da78a
Sarkis Varozian <svarozian@gmail.com>
2018-04-20 14:45:14 -0700

    outputs already strings

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "user_name" {
-  value = "${join("", module.chamber_user.user_name)}"
+  value = "${module.chamber_user.user_name}"
 }
 
 output "user_arn" {
-  value = "${join("", module.chamber_user.user_arn)}"
+  value = "${module.chamber_user.user_arn}"
 }
 
 output "user_unique_id" {
-  value = "${join("", module.chamber_user.user_unique_id)}"
+  value = "${module.chamber_user.user_unique_id}"
 }
 
 output "access_key_id" {
-  value = "${join("", module.chamber_user.access_key_id)}"
+  value = "${module.chamber_user.access_key_id}"
 }
 
 output "secret_access_key" {
-  value = "${join("", module.chamber_user.secret_access_key)}"
+  value = "${module.chamber_user.secret_access_key}"
 }
