3be9c97cdd07f18c8cc9cc0d3f1bfde68cf9a621
Jamie Nelson <jamie@theinstillery.com>
2018-05-19 14:34:07 +0100

    fixed race condition where kms key creation + policy creation, meant that the aws_iam_user_policy wssn't created because count couldn't be derived

--- main.tf
+++ main.tf
@@ -13,20 +13,44 @@ data "aws_iam_policy_document" "default" {
 
   statement {
     actions   = ["kms:Decrypt"]
-    resources = ["${var.kms_key_arn}"]
+    #resources = ["${var.kms_key_arn}"]
+    resources = ["${data.aws_kms_alias.kms_key.arn}"]
     effect    = "Allow"
   }
 }
 
-module "chamber_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.2.2"
-  namespace     = "${var.namespace}"
-  stage         = "${var.stage}"
-  name          = "${var.name}"
-  attributes    = "${var.attributes}"
-  tags          = "${var.tags}"
-  enabled       = "${var.enabled}"
-  force_destroy = "${var.force_destroy}"
+data "aws_kms_alias" "kms_key" {
+  name = "${var.kms_key_alias}" 
+}
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.5"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  attributes = "${var.attributes}"
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
+  enabled    = "${var.enabled}"
+}
+
+# Defines a user that should be able to write to you test bucket
+resource "aws_iam_user" "default" {
+  count         = "${var.enabled == "true" ? 1 : 0}"
+  name          = "${module.label.id}"
   path          = "${var.path}"
-  policy        = "${data.aws_iam_policy_document.default.json}"
+  force_destroy = "${var.force_destroy}"
+}
+
+# Generate API credentials
+resource "aws_iam_access_key" "default" {
+  count = "${var.enabled == "true" ? 1 : 0}"
+  user  = "${aws_iam_user.default.name}"
+}
+
+resource "aws_iam_user_policy" "default" {
+  count  = "${var.enabled == "true" ? 1 : 0}"
+  name   = "${module.label.id}"
+  user   = "${aws_iam_user.default.name}"
+  policy = "${data.aws_iam_policy_document.default.json}"
+  depends_on = ["data.aws_kms_alias.kms_key", "data.aws_iam_policy_document.default"]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,45 @@
+# output "user_name" {
+#   value       = "${module.chamber_user.user_name}"
+#   description = "Normalized IAM user name"
+# }
+
+# output "user_arn" {
+#   value       = "${module.chamber_user.user_arn}"
+#   description = "The ARN assigned by AWS for the user"
+# }
+
+# output "user_unique_id" {
+#   value       = "${module.chamber_user.user_unique_id}"
+#   description = "The user unique ID assigned by AWS"
+# }
+
+# output "access_key_id" {
+#   value       = "${module.chamber_user.access_key_id}"
+#   description = "The access key ID"
+# }
+
+# output "secret_access_key" {
+#   value       = "${module.chamber_user.secret_access_key}"
+#   description = "The secret access key. This will be written to the state file in plain-text"
+# }
+
+
 output "user_name" {
-  value       = "${module.chamber_user.user_name}"
-  description = "Normalized IAM user name"
+  value = "${join("", aws_iam_user.default.*.name)}"
 }
 
 output "user_arn" {
-  value       = "${module.chamber_user.user_arn}"
-  description = "The ARN assigned by AWS for the user"
+  value = "${join("", aws_iam_user.default.*.arn)}"
 }
 
 output "user_unique_id" {
-  value       = "${module.chamber_user.user_unique_id}"
-  description = "The user unique ID assigned by AWS"
+  value = "${join("", aws_iam_user.default.*.unique_id)}"
 }
 
 output "access_key_id" {
-  value       = "${module.chamber_user.access_key_id}"
-  description = "The access key ID"
+  value = "${join("", aws_iam_access_key.default.*.id)}"
 }
 
 output "secret_access_key" {
-  value       = "${module.chamber_user.secret_access_key}"
-  description = "The secret access key. This will be written to the state file in plain-text"
+  value = "${join("", aws_iam_access_key.default.*.secret)}"
 }
