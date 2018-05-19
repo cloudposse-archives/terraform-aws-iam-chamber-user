890d67501c87e348cacb8593451d869fcd866ecd
Jamie Nelson <jamie@theinstillery.com>
2018-05-19 14:57:23 +0100

    switched to terraform-terraform-label for modern sleekness

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ data "aws_kms_alias" "kms_key" {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.5"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.2"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
