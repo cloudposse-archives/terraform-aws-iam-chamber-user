8bebc6c367c10ce018ffe6c91c3a2ab7248eebf8
Igor Rodionov <goruha@gmail.com>
2018-05-21 23:51:54 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "chamber_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.3.0"  
+  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.3.0"
   namespace     = "${var.namespace}"
   stage         = "${var.stage}"
   name          = "${var.name}"
