068a383bc52abf66b8f7c4498ed17881a47f035b
Igor Rodionov <goruha@gmail.com>
2018-05-22 00:01:02 +0600

    Update version for system user module (#7)
    * Update version for system user module
    
    * Update version for system user module
    
    * Update main.tf
    
    * Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "chamber_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.2.2"
+  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.3.0"
   namespace     = "${var.namespace}"
   stage         = "${var.stage}"
   name          = "${var.name}"
@@ -28,5 +28,10 @@ module "chamber_user" {
   enabled       = "${var.enabled}"
   force_destroy = "${var.force_destroy}"
   path          = "${var.path}"
-  policy        = "${data.aws_iam_policy_document.default.json}"
+}
+
+resource "aws_iam_user_policy" "chamber_user" {
+  name   = "${module.chamber_user.user_name}"
+  user   = "${module.chamber_user.user_name}"
+  policy = "${data.aws_iam_policy_document.default.json}"
 }
